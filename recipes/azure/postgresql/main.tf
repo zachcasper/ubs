35c82fcbaca0f7dee09cb42edf484d4ae2ddb22d
Zach Casper <zachcasper@microsoft.com>
2025-05-06 19:06:48 -0500

    Increased azurerm provider version to current
    Signed-off-by: Zach Casper <zachcasper@microsoft.com>

--- recipes/azure/postgresql/main.tf
+++ recipes/azure/postgresql/main.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "=> 4.27.0"
     }
   }
-
-  required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
