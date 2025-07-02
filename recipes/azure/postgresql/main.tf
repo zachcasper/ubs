97b02f27fc345e9048b54c4414d1dbc5dccbd8d6
Zach Casper <zachcasper@microsoft.com>
2025-07-01 21:57:30 -0500

    Added storage_gb
    Signed-off-by: Zach Casper <zachcasper@microsoft.com>

--- recipes/azure/postgresql/main.tf
+++ recipes/azure/postgresql/main.tf
@@ -13,20 +13,41 @@ module "naming" {
 }
 
 variable "context" {
-  description = "This variable contains Radius recipe context."
+  description = "Context variable set by Radius which includes the Radius Application, Environment, and other Radius properties"
   type = any
+}
+
+variable "resource_group_name" {
+  description = "Azure Resource group set via a parameter on the Radius Recipe"
+  type = string
+}
+
+variable "location" {
+  description = "Azure region set via a parameter on the Radius Recipe"
+  type = string
+}
+
+variable "ha" {
+  description = "Create database with high availability or not"
+  type = boolean
+}
+
+variable "size_sku_map" {
+  type = map(string)
+  default = {
+    S = "GP_Standard_D2ds_v5"
+    M = "GP_Standard_D4ds_v5"
+    L = "GP_Standard_D8ds_v5"
+    XL = "GP_Standard_D16ds_v5"
+  }
 }
 
 resource "random_password" "password" {
   length           = 16
 }
 
-variable "resource_group_name" {
-  type = string
-}
-
-variable "location" {
-  type = string
+locals {
+  storage_mb == "" ? 32768 : var.context.resource.properties.storage_gb * 1024
 }
 
 resource "azurerm_postgresql_flexible_server" "todolist-db" {
@@ -35,9 +56,10 @@ resource "azurerm_postgresql_flexible_server" "todolist-db" {
   resource_group_name = var.resource_group_name
 
   administrator_login          = "postgres"
-  administrator_password = random_password.password.result
+  administrator_password       = random_password.password.result
 
-  sku_name   = "B_Standard_B1ms"
+  sku_name   = var.size_sku_map[var.context.resource.properties.size]
+  storage_mb = local.storage_mb
   version    = "16"
 
   zone = "1"
@@ -46,7 +68,7 @@ resource "azurerm_postgresql_flexible_server" "todolist-db" {
 # Set require_ssl to off
 resource "azurerm_postgresql_flexible_server_configuration" "disable_ssl" {
   name                = "require_secure_transport"
-  server_id         = azurerm_postgresql_flexible_server.todolist-db.id
+  server_id           = azurerm_postgresql_flexible_server.todolist-db.id
   value               = "off"
 }
 
